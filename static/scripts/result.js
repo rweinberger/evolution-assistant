1c83f6764afec43c57353de3affeea19b484c588
Rebecca Weinberger <rew@mit.edu>
2019-04-25 20:56:32 -0400

    Rework js to work with table

--- static/scripts/result.js
+++ static/scripts/result.js
@@ -1,32 +1,20 @@
 $(window).on('load', function() {
-    const resultToggle = $(".toggle-result");
-    const detailToggle = $(".toggle-detail");
+    const toggle = $(".toggle");
 
-    function toggleResult () {
-        console.log("toggleresult");
-        const result = $(this).siblings('.maintenance-result');
-        console.log(result);
-        if (result.css('display') == 'none') {
-            result.css("display", "block");
-        } else {
-            result.css("display", "none");
-        }
+    function toggleDetails () {
+        const targetName = $(this).attr('target');
+        const targets = $('.' + targetName);
+        targets.each(function() {
+            if ($(this).css('display') == 'none') {
+                $(this).css("display", "table-row");
+            } else {
+                $(this).css("display", "none");
+            }
+        })
     }
 
-    function toggleDetail () {
-        const detail = $(this).siblings('.maintenance-detail');
-        if (detail.css('display') == 'none') {
-            detail.css("display", "block");
-        } else {
-            detail.css("display", "none");
-        }
-    }
-
-    resultToggle.each(function() {
-        $(this).on("click", toggleResult)
+    toggle.each(function() {
+        $(this).on("click", toggleDetails)
     });
 
-    detailToggle.each(function() {
-        $(this).on("click", toggleDetail);
-    });
 })
